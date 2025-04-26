14a3aea91908b7afc481130f644858128abd5a62
ziad ahmed <167585034+ziad144@users.noreply.github.com>
2025-04-26 03:42:06 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Welocme to The user API'); 
+  res.send('Welocme to The Car Store API'); 
 });
 
 app.use("/users", userRouter);
